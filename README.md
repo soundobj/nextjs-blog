28b9582c8f6e365bed2c3e2794c2ce9c5fe51637
soundobj <soundobj@yahoo.com>
2020-12-02 15:29:15 +0000

    polish blog, API route

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 
 # Tailwind config VScode
 ```   
+  // .vscode/settings.json
   "tailwindCSS.includeLanguages": { "typescriptreact": "html", "plaintext": "javascript" },
   "tailwindCSS.emmetCompletions": true 
 ```
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,41 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import Link from 'next/link'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import Link from "next/link";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Tripple Dee</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-        <Link href="posts/first-post"><a>first post</a></Link>
+        <p className="py-3">Tripple Dee</p>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
-  )
+  );
 }
